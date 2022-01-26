6e8f17c94d5f6acd28077887fe7992c94409e4a5
NONAME CREATION(NNC) <abijithjr63@gmail.com>
2022-01-26 13:29:04 +0530

    some fix in ascii.py

--- ascii.py
+++ ascii.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python/
-import ascii_magic
+import ascii_magic, os
+
+#::::::::::: Requirments :::::::::
+print("\033[1;31mINSTALLING PACKAGES.... \033[0m")
+os.system("pip3.10 install ascii_magic")
+
+#/////////// Programe /////////////
 img=input("\033[1;34mENTER YOUR NAME OF IMAGE==> \033[0m")
 output = ascii_magic.from_image_file(img,colums=200,char="#")
 ascii_magic.to_terminal(output)
